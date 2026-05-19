67fddb82cd772789cc60742eede753568879cafc
xli1iax <108958756+xli1iax@users.noreply.github.com>
2026-05-19 17:05:35 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,10 +11,17 @@ use App\Services\DatabaseService;
 use App\Services\LogService;
 use App\Services\OctaveService;
 use Slim\Factory\AppFactory;
+use App\Services\AnimationUsageService;
+
+use App\Controllers\AnimationStatisticsController;
+use App\Services\AnimationStatisticsService;
 
 require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/config.php';
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 $app = AppFactory::create();
 
 $pdo = connectDatabase();
@@ -32,7 +39,69 @@ $casController = new CasController($octaveService, $logService);
 
 
 $animationService = new AnimationService($octaveService);
-$animationController = new AnimationController($animationService, $logService);
+$animationUsageService = new AnimationUsageService($pdo);
+$animationController = new AnimationController(
+    $animationService,
+    $logService,
+    $animationUsageService
+);
+
+$animationStatisticsService = new AnimationStatisticsService($pdo);
+$animationStatisticsController = new AnimationStatisticsController($animationStatisticsService);
+$app->get('/cas', function ($request, $response) {
+    ob_start();
+    require __DIR__ . '/../views/cas.php';
+    $html = ob_get_clean();
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
+$app->get('/animations/pendulum', function ($request, $response) {
+    ob_start();
+    require __DIR__ . '/../views/pendulum.php';
+    $html = ob_get_clean();
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
+$app->get('/animations/ball-beam', function ($request, $response) {
+    ob_start();
+    require __DIR__ . '/../views/ball-beam.php';
+    $html = ob_get_clean();
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
+$app->get('/statistics', function ($request, $response) {
+    ob_start();
+    require __DIR__ . '/../views/statistics.php';
+    $html = ob_get_clean();
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
+$app->get('/logs', function ($request, $response) {
+    ob_start();
+    require __DIR__ . '/../views/logs.php';
+    $html = ob_get_clean();
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
+$app->get('/documentation', function ($request, $response) {
+    ob_start();
+    require __DIR__ . '/../views/documentation.php';
+    $html = ob_get_clean();
+
+    $response->getBody()->write($html);
+    return $response;
+});
+
 
 $app->post('/api/animations/pendulum', [$animationController, 'invertedPendulum'])
     ->add(new ApiKeyMiddleware());
@@ -43,11 +112,29 @@ $app->post('/api/animations/ball-beam', [$animationController, 'ballBeam'])
 $app->post('/api/cas/execute', [$casController, 'execute'])
     ->add(new ApiKeyMiddleware());
 
+$app->get('/docs', function ($request, $response) {
+    ob_start();
+    require __DIR__ . '/../views/api-docs.php';
+    $html = ob_get_clean();
 
+    $response->getBody()->write($html);
+    return $response;
+});
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', [$homeController, 'index']);
-$app->post('/api/logs/export', [$logController, 'export']);
+$app->get('/api/logs', [$logController, 'index'])
+    ->add(new ApiKeyMiddleware());
+
+$app->get('/api/logs/export', [$logController, 'export'])
+    ->add(new ApiKeyMiddleware());
+
+$app->get('/api/statistics/animations', [$animationStatisticsController, 'summary'])
+    ->add(new ApiKeyMiddleware());
+
+$app->get('/api/statistics/animations/{name}', [$animationStatisticsController, 'details'])
+    ->add(new ApiKeyMiddleware());
+
 
 $app->run();
